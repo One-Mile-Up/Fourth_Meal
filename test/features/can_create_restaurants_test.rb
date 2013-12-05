b02ac836d90c0db3cb817d631ed293352459a167
Darryl Pequeen <pequickster@msn.com>
2013-12-05 14:31:38 -0700

    Add items to restaurant

--- test/features/can_create_restaurants_test.rb
+++ test/features/can_create_restaurants_test.rb
@@ -12,4 +12,19 @@ class CanCreateRestaurantsTest < Capybara::Rails::TestCase
     assert_content page, "Dive Bar"
   end
 
+  test "restaurant has only its items" do
+    restaurant1 = Restaurant.create(name: "Billy's BBQ")
+    restaurant2 = Restaurant.create(name: "Dive Bar")
+    item1 = Item.create(title: "Pork Sandwich", description: "Delicious", price: 5, restaurant_id: restaurant1.id)
+    item2 = Item.create(title: "PBR", description: "Hiptastic", price: 3, restaurant_id: restaurant2.id)
+
+    visit "/billys-bbq"
+    assert_content page, "Pork Sandwich"
+    refute_content page, "PBR"
+
+    visit "/dive-bar"
+    assert_content page, "PBR"
+    refute_content page, "Pork Sandwich"
+   end
+
 end
