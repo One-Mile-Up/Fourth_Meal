bd2ce245bfdf009da7960b89b080abf33f9c8470
Rolen Le <rolentle@gmail.com>
2013-12-05 08:56:34 -0700

    added dashboard testing

---
+++ test/features/dashboard_pages_work_test.rb
@@ -0,0 +1,29 @@
+require "./test/test_helper"
+
+class DashboardPageWorksTest < Capybara::Rails::TestCase
+
+  test "non admins user can not access dashboard" do
+    user = User.new(username: 'non admin', password: 'password', email: 'non_admin@example.com')
+    user.save
+
+    visit dashboard_path
+
+    refute_equal current_path, dashboard_path
+  end
+
+  test "admin user can access the dashboard" do
+    user = User.new({username: 'admin', password: 'password', email: 'admin@example.com'})
+    user.admin = true
+    user.save!
+
+    visit root_path
+    click_on "Login"
+
+    fill_in "Username", with: "admin"
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    visit dashboard_path
+    assert_equal dashboard_path, current_path
+  end
+end
