b189c5c830cbbe0f2c218e9446e479bd614538cb
Rolen Le <rolentle@gmail.com>
2013-12-06 13:58:11 -0700

    stashing stuff

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,10 @@ class OrderItem < ActiveRecord::Base
   belongs_to :item
 
   validates_numericality_of :quantity, greater_than: 0, only_integer: true
+
+  def increase_quantity
+      @quantity ||= 0
+      @quantity += 1
+      save
+  end
 end
