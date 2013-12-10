f6d7368c1956cb563d5ee66a668af7d7e6b4a69e
Rolen Le <rolentle@gmail.com>
2013-12-10 15:23:23 -0700

    Fixing Checkout For Heroku

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,17 +3,17 @@ class ChargesController < ApplicationController
     @order = current_user.orders.last
     @amount = @order.subtotal * 100
   end
-  
+
   def create
     # Amount in cents
     @order = current_user.orders.last
     @amount = @order.subtotal * 100
-  
+
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
       :card  => params[:stripeToken]
     )
-    begin 
+    begin
       charge = Stripe::Charge.create(
         :customer    => customer.id,
         :amount      => @amount.to_i,
@@ -24,10 +24,10 @@ class ChargesController < ApplicationController
       flash[:error] = e.message
     else
       current_user.change_order_to_completed
-      flash.notice = "Your order is successfull"
+      flash.notice = "Your order is successful"
       cookies.delete :order_id
       UserMailer.order_email(current_user, current_user.orders.last).deliver
     end
     redirect_to user_path(current_user)
-  end 
+  end
 end
