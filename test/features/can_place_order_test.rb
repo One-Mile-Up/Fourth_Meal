73d84a34d608c53844e280fe538d8de9f56c95c9
edatrix <lespath@gmail.com>
2013-12-13 15:24:22 -0700

    making more tests pass

--- test/features/can_place_order_test.rb
+++ test/features/can_place_order_test.rb
@@ -2,11 +2,32 @@ require './test/test_helper'
 require 'pry'
 class CanPlaceOrderTest < Capybara::Rails::TestCase
 
+  def login_user
+    @user = User.new
+    @user.username = 'user'
+    @user.password = 'password'
+    @user.email = 'user@example.com'
+    @user.save
+
+    visit root_path
+
+    click_on "Login"
+
+    fill_in "Username", with: 'user'
+    fill_in "Password", with: 'password'
+    click_button "Login"
+  end
+
 
   test "can add an item to the current order" do
-    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ")
+    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ", slug: "billys-bbq", status: "Approved")
     item = Item.create(title: 'Deviled Eggs', description: '12 luscious eggs', price: '1', restaurant: r1)
-    visit root_path
+
+    r1.items << item
+
+    login_user
+
+    visit restaurant_items_path(r1.slug)
     within("#item_1") do
       click_on "Add to Order"
     end
@@ -15,10 +36,14 @@ class CanPlaceOrderTest < Capybara::Rails::TestCase
   end
 
   test "cannot set the quantity to a negative number" do
-    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ")
+    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ", slug: "billys-bbq", status: "Approved")
     item = Item.create(title: 'Deviled Eggs', description: '12 luscious eggs', price: '1', restaurant_id: r1.id)
     order = Order.create
     order.items << item
+
+    r1.items << item
+
+    login_user
 
     visit order_path(order)
     fill_in 'order_item_quantity', with: '-5'
@@ -30,16 +55,21 @@ class CanPlaceOrderTest < Capybara::Rails::TestCase
 
 
   test "can add multiple items to order without logging in" do
-    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ")
-    r2 = Restaurant.find_or_create_by( name: "Dive Bar")
+    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ", slug: "billys-bbq", status: "Approved")
+    r2 = Restaurant.find_or_create_by( name: "Dive Bar", slug: "dive-bar", status: "Approved")
 
     item1 = Item.find_or_create_by(title: 'Deviled Eggs', description: '12 lucious eggs', price: '1', restaurant_id: r1.id)
     item2 = Item.find_or_create_by(title: 'Hard Boiled Eggs', description: '12 hard eggs', price: '1', restaurant_id: r2.id)
 
-    visit root_path
+    r1.items << item1
+    r2.items << item2
+
+    visit restaurant_items_path(r1.slug)
     within("#item_1") do
       click_on "Add to Order"
     end
+
+    visit restaurant_items_path(r2.slug)
     within("#item_2") do
       click_on "Add to Order"
     end
@@ -54,19 +84,22 @@ class CanPlaceOrderTest < Capybara::Rails::TestCase
   end
 
   test " items added to cart are seperated by restaurant" do
-    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ")
-    r2 = Restaurant.find_or_create_by( name: "Dive Bar")
+    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ", slug: "billys-bbq", status: "Approved")
+    r2 = Restaurant.find_or_create_by( name: "Dive Bar", slug: "dive-bar", status: "Approved")
 
     item1 = Item.find_or_create_by(title: 'Deviled Eggs', description: '12 lucious eggs', price: '1', restaurant_id: r1.id)
     item2 = Item.find_or_create_by(title: 'Hard Boiled Eggs', description: '12 hard eggs', price: '1', restaurant_id: r2.id)
 
+    r1.items << item1
+    r2.items << item2
+
     visit root_path
-    visit restaurant_path(r1.slug)
+    visit restaurant_items_path(r1.slug)
     within("#item_#{item1.id}") do
       click_on "Add to Order"
     end
 
-    visit restaurant_path(r2.slug)
+    visit restaurant_items_path(r2.slug)
     within("#item_#{item2.id}") do
       click_on "Add to Order"
     end
@@ -83,14 +116,13 @@ class CanPlaceOrderTest < Capybara::Rails::TestCase
   end
 
   test "can add multiple instances of same item to order" do
-    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ")
-    r2 = Restaurant.find_or_create_by( name: "Dive Bar")
+    r1 = Restaurant.find_or_create_by( name: "Billy's BBQ", slug: "billys-bbq", status: "Approved")
 
     item1 = Item.find_or_create_by(title: 'Deviled Eggs', description: '12 lucious eggs', price: '1', restaurant_id: r1.id)
-    item2 = Item.find_or_create_by(title: 'Hard Boiled Eggs', description: '12 hard eggs', price: '1', restaurant_id: r2.id)
 
+    r1.items << item1
 
-    visit root_path
+    visit restaurant_items_path(r1.slug)
     within("#item_1") do
       click_on "Add to Order"
     end
