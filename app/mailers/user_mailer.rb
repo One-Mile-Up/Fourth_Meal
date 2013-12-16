0131d0dea7b0b2e19d3885b0b216761bac086c3e
Rolen Le <rolentle@gmail.com>
2013-12-16 16:44:39 -0700

    Updated Emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -19,20 +19,26 @@ class UserMailer < ActionMailer::Base
     @user = user
     @restaurant = restaurant
     @url = "hunger-gains.herokuapp.com"
+    puts "Sending Approved Restaurant Email"
     mail(to: @user.email, subject: "Your new restaurant, #{@restaurant.name} is ready to take orders!")
+    puts "Sent Approved Restaurant Email"
   end
 
   def pending_restaurant_email(admin, restaurant)
     @admin = admin
     @restaurant = restaurant
     @url = "hunger-gains.herokuapp.com"
+    puts "Sending Pendin Restaurant Email"
     mail(to:admin.email, subject: "Restaurant: #{@restaurant.name} is waiting to be approved")
+    puts "Sent Pendin Restaurant Email"
   end
 
   def declined_restaurant_email(user, restaurant)
     @user = user
     @restaurant = restaurant
     @url = "hunger-gains.herokuapp.com"
+    puts "Sending Declined Restaurant Email"
     mail(to:user.email, subject: "Restaurant: #{@restaurant.name} has been declined")
+    puts "Sent Declined Restaurant Email"
   end
 end
